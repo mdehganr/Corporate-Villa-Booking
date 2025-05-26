84b0e6bd75702c33246cc5ff2a45e2882498f01a
Mohammad Dehghani <co_abzar@yahoo.com>
2025-05-26 10:30:20 -0700

    fix

--- src/app/data/Booking.ts
+++ src/app/data/Booking.ts
@@ -1,16 +1,16 @@
 export class Booking {
     id: number;
+    fullName: string;
     employeeEmail: string;
+    guest: string;
     startDate: string; // ISO 8601 format
     endDate: string;
-    fullName: string;
-    guest: string;
 
     constructor(id: number, fullName: string, employeeEmail: string, guest: string, startDate: string, endDate: string) {
         this.id = id;
         this.fullName = fullName;
-        this.guest = guest;
         this.employeeEmail = employeeEmail;
+        this.guest = guest;
         this.startDate = startDate;
         this.endDate = endDate;
     }
